09e1b6092237c55007a1409c6af5c547aa4dec49
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-18 14:52:51 +0000

    非ログインTopPageとログイン後TopPageを作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ログイン機能付きタスク管理サービス</title>
-    </head>
-    
-    <body>
+
+@extends('layout')
         <h1>ログイン</h1>
         <form action="/login" method="post">
             email:<input type="text" name="email"><br>
             パスワード:<input type="password" name="password"><br>
             <button>ログインする</button>
         </form>
-    </body>
-
-</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,11 @@ use App\Http\Controllers\AuthController;
 
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
+
+
+// タスク管理システム
+Route::get('/task/list', [TaskController::class, 'list']);
+
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
