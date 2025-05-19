29a2b154cf02d88dc58a41d720a933bc1cf8cafd
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-19 09:35:57 +0000

    formテスト用のコードを書いた

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,12 @@ use App\Http\Controllers\TaskController;
 
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
-
-
-// タスク管理システム
 Route::get('/task/list', [TaskController::class, 'list']);
-
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
+
+// form入力テスト用
+Route::get('/test', [TestController::class, 'index']);
+Route::post('/test/input', [TestController::class, 'input']);
