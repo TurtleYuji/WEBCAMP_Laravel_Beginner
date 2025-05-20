7c44c9bdd78fac8166b0b4a2d34bc447c9bb5a48
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-20 18:10:01 +0000

    タスクの一覧出力を書いた

--- app/Http/Requests/TaskRegisterPostRequest.php
+++ app/Http/Requests/TaskRegisterPostRequest.php
@@ -4,9 +4,11 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Models\Task as TaskModel;
 
 class TaskRegisterPostRequest extends FormRequest
 {
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -18,7 +20,7 @@ class TaskRegisterPostRequest extends FormRequest
             'name' => ['required', 'max:128'],
             'period' => ['required', 'date', 'after_or_equal:today'],
             'detail' => ['max:65535'],
-            'priority' => ['required', 'numeric', Rule::in([1, 2, 3]) ],
+            'priority' => ['required', 'numeric', Rule::in( array_keys(TaskModel::PRIORITY_VALUE) ) ],
         ];
     }
 }
